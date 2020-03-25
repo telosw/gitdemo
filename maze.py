94b5d5e85cf57094d6513c77a76ad9f022358b0e
wzpbuaa <18377067@buaa.edu.cn>
2020-03-25 16:28:49 +0800

    changed by wzpbuaa

--- maze.py
+++ maze.py
@@ -26,3 +26,44 @@ M=np.zeros((num_rows,num_cols,5),dtype=np.uint8)
 r,c=0,0
 history=[(0,0)]
 way=[]
+while history!=[]:
+    M[r,c,4]=1
+    choices=[]
+    if c>0 and M[r,c-1,4]==0:
+        choices.append('Le')
+    if r>0 and M[r-1,c,4]==0:
+        choices.append('Up')
+    if c<num_cols-1 and M[r,c+1,4]==0:
+        choices.append('Ri')
+    if r<num_rows-1 and M[r+1,c,4]==0:
+        choices.append('Do')
+    if len(choices)!=0:
+        history.append([r,c])
+        way.append([r,c])
+        direction=random.choice(choices)
+        if direction=='Le':
+            M[r,c,0]=1
+            r,c=r,c-1
+            M[r,c,2]=1
+        if direction=='Up':
+            M[r,c,1]=1
+            r,c=r-1,c
+            M[r,c,3]=1
+        if direction=='Ri':
+            M[r,c,2]=1
+            r,c=r,c+1
+            M[r,c,0]=1
+        if direction=='Do':
+            M[r,c,3]=1
+            r,c=r+1,c
+            M[r,c,1]=1
+    else:
+        way.append([r,c])
+        r,c=history.pop()
+M[0,0,0]=1
+M[num_rows-1,num_cols-1,2]=1
+for i in range(len(way)):
+    if way[i] == [num_rows-1,num_cols-1]:
+        way=way[:i+1]
+        break
+way=find_way(way)
